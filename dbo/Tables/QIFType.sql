6e516d6bb896ea0b30e8b5b61afc4825d8d4ecd7
russ <hartr.h16@gmail.com>
2023-12-07 01:32:30 -0500

    add account type

--- dbo/Tables/QIFType.sql
+++ dbo/Tables/QIFType.sql
@@ -1,6 +1,8 @@
 CREATE TABLE [dbo].[QIFType] (
-    [QIFTypeID] INT           IDENTITY (1, 1) NOT NULL,
-    [QIFType]   NVARCHAR (10) NOT NULL
+    [QIFTypeID]   INT           IDENTITY (1, 1) NOT NULL,
+    [QIFType]     NVARCHAR (10) NOT NULL,
+    [AccountType] NVARCHAR (10) NULL,
+    CONSTRAINT [PK_QIFType] PRIMARY KEY CLUSTERED ([QIFTypeID] ASC)
 );
 GO
 
