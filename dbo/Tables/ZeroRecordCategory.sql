7aaa7984cea49b10c964c2ac43d31f35b434e621
russ <hartr.h16@gmail.com>
2024-06-23 23:05:00 -0400

    open month flag

--- dbo/Tables/ZeroRecordCategory.sql
+++ dbo/Tables/ZeroRecordCategory.sql
@@ -7,11 +7,13 @@ CREATE TABLE [dbo].[ZeroRecordCategory] (
     [ExpectedCategoryTypeID2]    INT            NULL,
     [ExpectedCategoryID2]        INT            NULL,
     [ExpectedCategory2NextMonth] BIT            NULL,
+    [OpenMonthFlag]              BIT            CONSTRAINT [DF_ZeroRecordCategory_OpenMonthFlag] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_ZeroRecordCategory] PRIMARY KEY CLUSTERED ([ZeroRecordCategoryID] ASC),
     CONSTRAINT [FK_ZeroRecordCategory_Category] FOREIGN KEY ([ExpectedCategoryID2]) REFERENCES [dbo].[Category] ([CategoryID]),
     CONSTRAINT [FK_ZeroRecordCategory_CategoryType] FOREIGN KEY ([ExpectedCategoryTypeID1]) REFERENCES [dbo].[CategoryType] ([CategoryTypeID]),
     CONSTRAINT [FK_ZeroRecordCategory_CategoryType1] FOREIGN KEY ([ExpectedCategoryTypeID2]) REFERENCES [dbo].[CategoryType] ([CategoryTypeID])
 );
 GO
+
 
 
