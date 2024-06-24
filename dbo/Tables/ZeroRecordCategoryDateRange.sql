8d4da873ef1075a4dd7984b49a7a679239b3a3ba
russ <hartr.h16@gmail.com>
2024-06-23 21:48:18 -0400

    add notes to zr category date ranges

--- dbo/Tables/ZeroRecordCategoryDateRange.sql
+++ dbo/Tables/ZeroRecordCategoryDateRange.sql
@@ -1,8 +1,11 @@
 CREATE TABLE [dbo].[ZeroRecordCategoryDateRange] (
-    [ZeroRecordCategoryDateRangeID] INT  IDENTITY (1, 1) NOT NULL,
-    [ZeroRecordCategoryID]          INT  NOT NULL,
-    [FromDate]                      DATE NOT NULL,
-    [ThruDate]                      DATE NULL
+    [ZeroRecordCategoryDateRangeID] INT            IDENTITY (1, 1) NOT NULL,
+    [ZeroRecordCategoryID]          INT            NOT NULL,
+    [FromDate]                      DATE           NOT NULL,
+    [ThruDate]                      DATE           NULL,
+    [Notes]                         NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_ZeroRecordCategoryDateRange] PRIMARY KEY CLUSTERED ([ZeroRecordCategoryDateRangeID] ASC),
+    CONSTRAINT [FK_ZeroRecordCategoryDateRange_ZeroRecordCategory] FOREIGN KEY ([ZeroRecordCategoryID]) REFERENCES [dbo].[ZeroRecordCategory] ([ZeroRecordCategoryID])
 );
 GO
 
