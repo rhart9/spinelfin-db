408dee122c8f3546a6bc98f2ffacd8edb5675201
russ <hartr.h16@gmail.com>
2024-07-14 01:40:05 -0400

    flag to use week or month boundaries

--- dbo/Tables/ZeroRecordCategory.sql
+++ dbo/Tables/ZeroRecordCategory.sql
@@ -15,12 +15,14 @@ CREATE TABLE [dbo].[ZeroRecordCategory] (
     [SplitDescription1]                 NVARCHAR (1024) NULL,
     [SplitDescription2]                 NVARCHAR (1024) NULL,
     [Multiplier]                        INT             CONSTRAINT [DF_ZeroRecordCategory_Multiplier] DEFAULT ((1)) NOT NULL,
+    [UseWeekBoundaries]                 BIT             CONSTRAINT [DF_ZeroRecordCategory_UseWeekBoundaries] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_ZeroRecordCategory] PRIMARY KEY CLUSTERED ([ZeroRecordCategoryID] ASC),
     CONSTRAINT [FK_ZeroRecordCategory_Category] FOREIGN KEY ([ExpectedCategoryID2]) REFERENCES [dbo].[Category] ([CategoryID]),
     CONSTRAINT [FK_ZeroRecordCategory_CategoryType] FOREIGN KEY ([ExpectedCategoryTypeID1]) REFERENCES [dbo].[CategoryType] ([CategoryTypeID]),
     CONSTRAINT [FK_ZeroRecordCategory_CategoryType1] FOREIGN KEY ([ExpectedCategoryTypeID2]) REFERENCES [dbo].[CategoryType] ([CategoryTypeID])
 );
 GO
+
 
 
 
